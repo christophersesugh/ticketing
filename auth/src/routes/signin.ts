5cda7e987d618b2111c11db26c46c00bf8322166
casdevlabs <cas737185@gmail.com>
2024-02-20 17:10:37 +0100

    add currentuser route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from "express";
+import express, { Request as Req, Response as Res } from "express";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi there current user!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validationResult,
+  async (req: Req, res: Res) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    console.log("step 2");
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password,
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    console.log("step 3");
+
+    const token = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET!,
+    );
+    console.log("step 4");
+
+    req.session = { jwt: token };
+    console.log("step 5");
+    res.status(200).send(existingUser);
+  },
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,6 +5,7 @@ import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { Password } from "../services/password";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -17,11 +18,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Req, res: Res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
